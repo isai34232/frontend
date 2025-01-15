125e5e14300319752bb5ee9aa73643207a6760bc
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2025-01-14 23:10:58 -0600

    update error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,24 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Rutas específicas con renderizado dinámico
+  {
+    path: 'editProducto/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'editP/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'comprar/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'venderD/:id',
+    renderMode: RenderMode.Client
+  },
+  // Ruta por defecto para el resto
   {
     path: '**',
     renderMode: RenderMode.Prerender
